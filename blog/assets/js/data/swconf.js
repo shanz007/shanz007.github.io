0cd00a2266de0d79f90b4537cd8c3227a7c551e4
shanz007 <shanz007@users.noreply.github.com>
2025-12-07 17:20:03 +0000

    deploy: shanz007/nl-hugo-blowfish-portfolio@f0f2b11a835345dc7f339ea56741fed4788fd619

---
+++ blog/assets/js/data/swconf.js
@@ -0,0 +1,32 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1765127998',resources: [
+      '/blog/assets/css/jekyll-theme-chirpy.css',
+      '/blog/',
+      
+        '/blog/categories/',
+      
+        '/blog/tags/',
+      
+        '/blog/archives/',
+      
+        '/blog/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+          'https://shanz007.goatcounter.com/counter/'
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
